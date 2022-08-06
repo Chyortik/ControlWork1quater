c04cc8c5317471608fdd5cb32f4a820dc30a74dd
Dmitryi <dimon_smirnov81@mail.ru>
2022-08-06 14:13:36 +0300

    Релизуем метод вывода полученного массива на экран

--- Program.cs
+++ Program.cs
@@ -27,3 +27,34 @@ int CheckArray(string[] array, int lengthLimit)
     }
     return result;
 }
+
+
+FillNewArray(StringsArray, StringsArray1, lengthLimit);
+Console.WriteLine($"{PrintArray(StringsArray1)}");
+
+void FillNewArray(string[] oldArray, string[] newArray, int lengthLimit)
+{
+    int temp = 0;
+    for (int i = 0; i < oldArray.Length; i++)
+    {
+        if (oldArray[i].Length <= lengthLimit)
+        {
+            newArray[temp] = oldArray[i];
+            temp++;
+        }
+    }
+}
+
+
+string PrintArray(string[] array)
+{
+    string result = string.Empty;
+    result = "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += $"{array[i],1}";
+        if (i < array.Length - 1) result += ", ";
+    }
+    result += "]";
+    return result;
+}
