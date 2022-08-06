2a04c64d6e819da52dc297cd350d8c4e40805387
Dmitryi <dimon_smirnov81@mail.ru>
2022-08-06 14:07:41 +0300

    Реализуем ввод данных пользователем

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string[] StringsArray = new string[] {};
+int n = InputNumbers("Введите количество элементов массива: ");
+StringsArray = new string[n];
+for (int i = 0; i < StringsArray.Length; i++)
+{
+    Console.Write($" Введите {i + 1} строку: ");
+    StringsArray[i] = Console.ReadLine()!;
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
